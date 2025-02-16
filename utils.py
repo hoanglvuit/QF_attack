d2023b15ed1adbe0770234cb53fbcff57e33b746
hoanglvuit <hoanglv2824@gmail.com>
2025-02-16 09:49:38 +0700

    fixbeam

--- utils.py
+++ utils.py
@@ -61,9 +61,9 @@ def search_min_sentence_iteration(sentence, char_list, length, iter_times, mask=
     for i in range(length):
         modify_sentences = [sentence + ' ' for sentence in modify_sentences]
         modify_sentences = search_min_char(sentence_embedding, modify_sentences, char_list, -1, tokenizer=tokenizer, text_encoder=text_encoder,top_k = 1)
-    modify_sentence = modify_sentences[:1] 
-    modify_sentences = [modify_sentence]
-    print(modify_sentences)
+    modify_sentences = modify_sentences[:1] 
+    # modify_sentences = [modify_sentence]
+    # print(modify_sentences)
     for i in range(iter_times):
         for k in range(length, 0, -1):
             modify_sentences = search_min_char(sentence_embedding, modify_sentences, char_list, -k, mask, tokenizer=tokenizer, text_encoder=text_encoder,top_k = 1)
